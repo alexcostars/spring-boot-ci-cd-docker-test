43c427b73ea13d5f6bedd8d8ee2bc0a1d3a82bd0
Alex Costa <alexcostars@gmail.com>
2021-09-07 13:48:26 -0300

    first commit

---
+++ src/test/java/com/example/jenkinscicd/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinscicd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
